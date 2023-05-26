52552a5a9cf1c7450cc024403b4b7d18d2541617
Jake Wood <jakewood@Jakes-MacBook-Pro.local>
2023-05-26 21:23:59 +0100

    added split text to hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
 import { useEffect, useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './components/globals/Navbar/Navbar'
 import Hero from './components/elements/Hero/Hero'
 import './App.css'
 import TextBlock from './components/elements/TextBlock/TextBlock'
+import SplitType from "split-type";
+import { gsap } from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger)
 
 function App() {
   const [count, setCount] = useState(0)
   const [navbarHeight, setNavbarHeight] = useState("");
 
+  // Refs for GSAP Animations
   let ref = useRef();
+  let overviewSectionRef = useRef();
+  let textToSplitRef = useRef();
 
   useEffect(() => {
     setNavbarHeight(ref.current.offsetHeight)
   }, [navbarHeight]);
 
+  useEffect(() => {
+    
+    let splitText = new SplitType(textToSplitRef.current, { types: "lines" })
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: overviewSectionRef.current,
+        start: "top bottom",
+        end: "top top",
+        markers: true,
+        scrub: 2
+      }
+    });
+  }, []);
+
   return (
     <main className="container mx-auto">
       <Navbar navbarEl={ref} />
       <Hero navbarHeight={navbarHeight} />
-      <section className="overview min-h-screen flex justify-center items-center">
-        <h2 className="flex flex-col items-center text-center text-6xl font-light gap-4 uppercase">
+      <section className="overview min-h-screen flex justify-center items-center" ref={overviewSectionRef}>
+        <h2 className="flex flex-col items-center text-center text-6xl font-light gap-4 uppercase" ref={textToSplitRef}>
           <p>digital experiences</p>
           <p>designed with <span className="font-aloha font-regular text-[60px] tracking-wider">elegance</span>,</p>
           <p>built with <span className="uppercase font-bold">impact</span></p>
--- src/components/globals/Navbar/Navbar.jsx
+++ src/components/globals/Navbar/Navbar.jsx
@@ -1,36 +1,78 @@
+import { v4 as uuidv4 } from "uuid"
+
 const Navbar = ({ navbarEl }) => {
-  return <nav className="navbar flex justify-between items-center py-5" ref={navbarEl}>
+
+  const navLinks = [
+    {
+        menu: "Site Navigation",
+        links: [
+            {
+                label: "Home",
+                href: "",
+                isButton: false,
+                openInNewTab: false,
+            },
+            {
+                label: "About",
+                href: "",
+                isButton: false,
+                openInNewTab: false,
+            },
+            {
+                label: "Work",
+                href: "",
+                isButton: false,
+                openInNewTab: false,
+            },
+            {
+                label: "Blog",
+                href: "",
+                isButton: false,
+                openInNewTab: false,
+            }
+        ]
+    },
+    {
+        menu: "Socials",
+        links: [
+            {
+                label: "LinkedIn",
+                href: "",
+                isButton: false,
+                openInNewTab: true,
+            },
+            {
+                label: "Dribbble",
+                href: "https://dribbble.com/jakealistairwood",
+                isButton: false,
+                openInNewTab: true,
+            },
+            {
+                label: "Contact",
+                href: "",
+                isButton: true,
+                openInNewTab: false
+            }
+        ]
+    }
+  ]  
+
+  return <nav className="navbar container sticky top-0 flex justify-between items-center py-5" ref={navbarEl}>
     <a className="navbar__logo uppercase font-bold text-" href="#">Jake Alistair Wood</a>
-    <ul className="navbar__nav-links flex justify-between items-center gap-5 uppercase text-xs">
-        <li className="navbar__nav-links">
-            <a href="">Home</a>
-        </li>
-        <li className="navbar__nav-links">
-            <a href="">About</a>
-        </li>
-        <li className="navbar__nav-links">
-            <a href="">Work</a>
-        </li>
-        <li className="navbar__nav-links">
-            <a href="">Blog</a>
-        </li>
-    </ul>
-    <ul className="navbar__nav-links flex justify-between items-center gap-5 uppercase text-xs">
-        <li className="navbar__nav-links">
-            <a href="">LinkedIn</a>
-        </li>
-        <li className="navbar__nav-links">
-            <a href="flex flex-col relative btn">
-                <span className="block opacity-0">Dribbble</span>
-                <span className="block absolute">Dribbble</span>
-                <span className="block absolute">Dribbble</span>
-            </a>
-        </li>
-        <li className="navbar__nav-links border border-black rounded-full py-2 px-4">
-            <a href="">Contact</a>
-        </li>
-    </ul>
-  </nav>
+    {navLinks.map(menu => {
+        return <ul key={uuidv4()} className="navbar__nav-links flex justify-between items-center gap-5 uppercase text-xs">
+            {menu.links.map(link => {
+                return <li key={uuidv4()} className={`navbar__nav-link ${link.isButton && "btn border border-black rounded-full py-2 px-4"}`}>
+                    <a href={link.href} className="flex flex-col relative text-animate-wrapper" target={`${link.openInNewTab ? "_blank" : "_self"}`}>
+                        <span className="block opacity-0 width-placeholder">{link.label}</span>
+                        <span className="block absolute line line--one">{link.label}</span>
+                        <span className="block absolute line line--two">{link.label}</span>
+                    </a>
+                </li>
+            })}
+        </ul>
+    })}
+    </nav>
 }
 
 export default Navbar
